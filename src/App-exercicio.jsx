fc98abf915253b6aab499fb4cb5ec18c884a3383
izacesar <izadora.cesar@aluno.senai.br>
2026-05-20 08:11:31 -0300

    alterações

--- src/App-exercicio.jsx
+++ src/App-exercicio.jsx
@@ -6,7 +6,7 @@ import MensagemSecreta from "./componentes/MensagemSecreta/MensagemSecreta";
 const App = () => {
   return (
      <fragment>
-       <BoasVindas nome = "Livia"/>
+       <BoasVindas nome = "Izadora"/>
 
        <MensagemSecreta mensagem = {true}/>
        <MensagemSecreta/>
@@ -16,21 +16,21 @@ const App = () => {
             <CartaoProduto
             nome = "Pizza"
             descricao = "Pizza de Calabresa com borda recheada"
-            preco = "R$ 33,00"
+            preco = "R$ 40,00"
             imagem = "Img-pizza.png"
             />
 
             <CartaoProduto
             nome = "Hamburguer"
             descricao = "Hamburguer artesanal com queijo"
-            preco = "R$ 25,00"
+            preco = "R$ 30,00"
              imagem = "Img-burguer.png"
             />
 
             <CartaoProduto
             nome = "Milk Shake"
             descricao = "Milk Shake de chocolate"
-            preco = "R$ 18,00"
+            preco = "R$ 15,00"
             imagem = "Img-shake.png"
             />
       </div>
